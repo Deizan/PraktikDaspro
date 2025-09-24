12ea8925aafc648773a17d1dd63ca972aef2d671
Deizan <deyzen1107@gmail.com>
2025-09-24 15:23:37 +0700

    hallo to

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama saya di java");
-         
+        System.out.println("Hi, ini baris kode pertama");
+
     }
 
 }
